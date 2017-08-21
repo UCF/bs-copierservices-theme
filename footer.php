639b3e540e1b087b17406ced51cc2cd421fbd10b
Mike Setzer <michaelsetzerucf@gmail.com>
2017-08-21 10:34:52 -0400

    Post ID Change

--- footer.php
+++ footer.php
@@ -5,25 +5,25 @@
 			<div class="center">
 
 				<div class="footer-logo"><img src="<?php bloginfo('template_directory'); ?>/library/images/home-logo.png" alt="copier services and support" /></div>
-					
+
 				<?php wp_nav_menu(array(
 					'theme_location' => 'main-nav',
 					'container' => ''
 				)); ?>
 
 				<div class="clear"></div>
-				
+
 			</div>
 		</div>
 
 		<div class="footer-bottom">
 			<div class="center">
-				
-				<div class="copy"><?php the_field('footer_copy', 6); ?></div>
+
+				<div class="copy"><?php the_field('footer_copy', 108); ?></div>
 
 			</div>
 		</div>
-		
+
 	</div>
 
 	<?php wp_footer(); ?>
