ef945a4bfcbb8f1c6885f7c1797f42e6f3d0c5ae
Mike Setzer <michaelsetzerucf@gmail.com>
2017-08-24 09:41:44 -0400

    Adding banner

--- pagebanner.php
+++ pagebanner.php
@@ -10,7 +10,8 @@
 				<a href="<?php echo site_url(); ?>"><img src="<?php bloginfo('template_directory'); ?>/library/images/home-logo.png" alt="copier services and support" /></a>
 
 			<?php } else { ?>
-
+				
+				<a href="<?php echo site_url(); ?>"><img src="<?php bloginfo('template_directory'); ?>/library/images/home-logo.png" alt="copier services and support" /></a>
 				<h1><a href="<?php echo site_url(); ?>"><?php the_title(); ?></a></h1>
 
 			<?php } ?>
